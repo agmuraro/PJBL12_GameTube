acd26ae33d669337747cc654a1dff576ad132738
Joao <joaovitoras146@gmail.com>
2023-05-30 22:51:16 -0300

    Classe Game

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,31 @@
 package src;
 
 public class Game {
+    private String name;
+    private String descricao;
+    private Double preco;
+    private Image image;
+            
+    public Game(String name, String descricao, Double preco, Image image) {
+        this.name = name;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.image = image
+    }
+    
+    public void getName { return name }
+
+    public void setName(String name) { this.name = name }
+
+    public void getDescricao { return descricao }
+
+    public void setDescricao(String descricao) { this.descricao = descricao }
+
+    public void getPrice { return price }
+
+    public void setPrice(Double price) { this.price = price }
+
+    public void getImage { return image }
+
+    public void setImage(Image image) { this.image = image }
 }
