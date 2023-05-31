b349ecda87d93bdb193b58d3b5407cbda2de6cb8
Joao <joaovitoras146@gmail.com>
2023-05-31 09:52:02 -0300

    Arruma classe game

--- src/Game.java
+++ src/Game.java
@@ -4,23 +4,22 @@ public class Game {
     private String name;
     private String descricao;
     private Double preco;
-    private Image image;
             
-    public Game(String name, String descricao, Double preco, Image image) {
+    public Game(String name, String descricao, Double preco) {
         this.name = name;
         this.descricao = descricao;
         this.preco = preco;
     }
     
-    public void getName() { return name }
+    public void getName() { return name; }
 
-    public void setName(String name) { this.name = name }
+    public void setName(String name) { this.name = name; }
 
-    public void getDescricao() { return descricao }
+    public void getDescricao() { return descricao; }
 
-    public void setDescricao(String descricao) { this.descricao = descricao }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
 
-    public void getPrice() { return price }
+    public void getPrice() { return price; }
 
-    public void setPrice(Double price) { this.price = price }
+    public void setPrice(Double price) { this.price = price; }
 }
