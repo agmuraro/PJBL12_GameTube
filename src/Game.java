c323de66861d4688ffb38d789586197213960424
Joao <joaovitoras146@gmail.com>
2023-05-31 09:53:12 -0300

    Arruma classe game

--- src/Game.java
+++ src/Game.java
@@ -11,15 +11,15 @@ public class Game {
         this.preco = preco;
     }
     
-    public void getName() { return name; }
+    public String getName() { return name; }
 
     public void setName(String name) { this.name = name; }
 
-    public void getDescricao() { return descricao; }
+    public String getDescricao() { return descricao; }
 
     public void setDescricao(String descricao) { this.descricao = descricao; }
 
-    public void getPrice() { return price; }
+    public Double getPrice() { return price; }
 
     public void setPrice(Double price) { this.price = price; }
 }
