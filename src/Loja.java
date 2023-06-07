841b48fe73e9788041a2fa462c1586be112701e4
Birckholz <100889867+Birckholz@users.noreply.github.com>
2023-06-06 21:26:37 -0300

    Inicio Listagem Loja

--- src/Loja.java
+++ src/Loja.java
@@ -1,11 +1,15 @@
 package src;
 
 import org.json.JSONObject;
+import org.json.*;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Loja extends JFrame{
     private JSONObject session;
@@ -89,7 +93,54 @@ public class Loja extends JFrame{
             System.out.println(e.getMessage());
             descartar();
         }
+        readGameListingsFromFile("src/games.json");
+    }
+    private void readGameListingsFromFile(String filePath) {
+        try {
+            String fileContent = new String(Files.readAllBytes(Paths.get(filePath)));
+            JSONArray gameListingsArray = new JSONArray(fileContent);
 
+            for (int i = 0; i < gameListingsArray.length(); i++) {
+                JSONObject listingObject = gameListingsArray.getJSONObject(i);
+                String imagePath = listingObject.getString("directory");
+                String name = listingObject.getString("name");
+
+                // Load the image from the file
+                ImageIcon imageIcon = new ImageIcon(imagePath);
+                Image image = imageIcon.getImage();
+                Image scaledImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+                ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
+
+                // Create a panel to hold the image, name, and button
+                JPanel gamePanel = new JPanel();
+                gamePanel.setLayout(new BoxLayout(gamePanel, BoxLayout.Y_AXIS));
+                gamePanel.setBackground(Color.WHITE); // Set background color to white
+
+                // Create a JLabel to display the image
+                JLabel imageLabel = new JLabel();
+                imageLabel.setIcon(scaledImageIcon);
+                gamePanel.add(imageLabel);
+
+                // Create a JLabel to display the name
+                JLabel nameLabel = new JLabel(name);
+                nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                gamePanel.add(nameLabel);
+
+                // Create a button
+                JButton pagarButton = new JButton("Pagar");
+                pagarButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+                gamePanel.add(pagarButton);
+
+                // Add the game panel to the content pane
+                getContentPane().add(gamePanel);
+            }
+
+            // Update the content pane layout after adding all game panels
+            getContentPane().setLayout(new GridLayout(0, 3, 10, 10));
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
     }
     public void descartar() {
         dispose();
